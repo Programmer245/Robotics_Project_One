44cd149f6b83678eb0b87aa97e790dcc592ea843
Programmer245 <brunantony24@gmail.com>
2024-04-09 17:49:12 +0200

    Added initial files and partially completed first node

---
+++ src/gps_to_odom.cpp
@@ -0,0 +1,36 @@
+/**
+ * Computes odometry from gps data. 
+ * Subscribes to /fix topic to get gps data as sensor_msgs/NavSatFix objects.
+ * Converts sensor_msgs/NavSatFix objects to nav_msgs/Odometry objects.
+ * Publishes converted data to gps/odom topic
+ * 
+ * @author anto
+*/
+
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
+#include "sensor_msgs/NavSatFix.h"
+
+class pub_sub {
+    private:
+        ros::NodeHandle n;
+
+        ros::Subscriber sub;
+        ros::Publisher pub;
+    public:
+        pub_sub() {
+            sub = n.subscribe("/fix", 1, &pub_sub::callback, this);
+            pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
+        }
+
+        void callback(const sensor_msgs::NavSatFix::ConstPtr &data) {
+            ROS_INFO("Callback triggered.");
+        }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "gps_to_odom");
+    pub_sub my_pub_sub;
+    ros::spin();
+    return 0;
+}
