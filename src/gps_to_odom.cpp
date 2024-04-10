368d7cb547aaee58c821b8bc4bd0c6e93ebe7be4
Programmer245 <brunantony24@gmail.com>
2024-04-10 20:43:14 +0200

    Fixed error with usage of e value

--- src/gps_to_odom.cpp
+++ src/gps_to_odom.cpp
@@ -4,6 +4,8 @@
  * Converts sensor_msgs/NavSatFix objects to nav_msgs/Odometry objects.
  * Publishes converted data to gps/odom topic
  * 
+ * Remember to start the launch file first (to get the initial parameters)
+ * 
  * @author anto, abdo
 */
 
@@ -12,7 +14,9 @@
 #include "sensor_msgs/NavSatFix.h"
 #include <cmath>
 
-#define a 6378137 // Equatorial radius
+double a = 6378137; // Equatorial radius in m
+double b = 6356752; // Polar radius in m
+double e_square = 1-pow(a/b,2); // First numerical eccentricity
 
 class pub_sub {
     private:
@@ -28,19 +32,20 @@ class pub_sub {
             n.getParam("lon_r", startLon);
             n.getParam("lat_r", startLat);
 
-            initialECEF = LatLonToECEF(startAlt, startLon, startLat); // Convert the initial altitude, longitude, and latitude to ECEF
+            initialECEF = AltLonLatToECEF(startAlt, startLon, startLat); // Convert the initial altitude, longitude, and latitude to ECEF
 
             sub = n.subscribe("/fix", 1, &pub_sub::callback, this);
             pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
         }
 
         void callback(const sensor_msgs::NavSatFix::ConstPtr &data) {
-            std::array<double, 3> newCoordinates = LatLonToECEF(data->altitude, data->longitude, data->latitude); // Gets the new ECEF coordinates
-            ROS_INFO("Callback triggered.");
+            std::array<double, 3> newCoordinates = AltLonLatToECEF(data->altitude, data->longitude, data->latitude); // Gets the new ECEF coordinates
         }
 
         //The 3 parameters of this method are in degrees
-        std::array<double, 3> LatLonToECEF(double alt, double lon, double lat) {
+        std::array<double, 3> AltLonLatToECEF(double alt, double lon, double lat) {
+            ROS_INFO("Received parameters: alt: %lf, long: %lf, lat: %lf", alt, lon, lat); // TODO: Must delete
+
             std::array<double, 3> res;
 
             double latRad = lat * M_PI/180;
@@ -50,20 +55,22 @@ class pub_sub {
 
             res[0] = (nResult + alt) * cos(latRad) * cos(lonRad);
             res[1] = (nResult + alt) * cos(latRad) * sin(lonRad);
-            res[2] = (nResult * (1 - exp(2)) + alt) * sin(latRad);
+            res[2] = (nResult * (1 - e_square) + alt) * sin(latRad);
+
+            ROS_INFO("Converted parameters: X: %lf, Y: %lf, Z: %lf", res[0], res[1], res[2]); // TODO: Must delete
 
             return res; // Contains XECEF, YECEF, ZECEF
         }
 
         //The parameter is in radiants
         double N(double latRad) {
-            return a/sqrt(1 - (exp(2) * pow(sin(latRad), 2)));
+            return a/sqrt(1-e_square*pow(sin(latRad),2));
         }
 };
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "gps_to_odom");
-    pub_sub my_pub_sub();
+    pub_sub my_pub_sub;
     ros::spin();
     return 0;
 }
