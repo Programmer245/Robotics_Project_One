320ba05d83da9ea703d0949c95d9c7e533d2b8ca
Programmer245 <brunantony24@gmail.com>
2024-04-10 22:44:30 +0200

    Added ENU conversion to first node

--- src/gps_to_odom.cpp
+++ src/gps_to_odom.cpp
@@ -6,13 +6,14 @@
  * 
  * Remember to start the launch file first (to get the initial parameters)
  * 
- * @author anto, abdo
+ * @author anto, abdo, jie
 */
 
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/NavSatFix.h"
 #include <cmath>
+#include <tf/transform_datatypes.h>
 
 double a = 6378137; // Equatorial radius in m
 double b = 6356752; // Polar radius in m
@@ -23,28 +24,56 @@ class pub_sub {
         ros::NodeHandle n;
         ros::Subscriber sub;
         ros::Publisher pub;
-        std::array<double, 3> initialECEF; // Stores the initial ECEF coordinates of the robot
+
+        std::array<double, 3> initialECEF; // Stores the initial X,Y,Z ECEF coordinates of the robot
+        std::array<double, 3> initialAltLonLat; // Stores the initial altitude, longitude, and latitude of the robot in degrees
+        std::array<double, 3> oldENU; // The X,Y,Z ENU of the previous position 
     public:
         pub_sub() {
-            double startAlt, startLon, startLat;
+            n.getParam("alt_r", initialAltLonLat[0]); // Reads the parameters containing the initial altitude, longitude, and latitude of the robot
+            n.getParam("lon_r", initialAltLonLat[1]);
+            n.getParam("lat_r", initialAltLonLat[2]);
 
-            n.getParam("alt_r", startAlt); // Reads the parameters containing the initial altitude, longitude, and latitude of the robot
-            n.getParam("lon_r", startLon);
-            n.getParam("lat_r", startLat);
-
-            initialECEF = AltLonLatToECEF(startAlt, startLon, startLat); // Convert the initial altitude, longitude, and latitude to ECEF
+            initialECEF = AltLonLatToECEF(initialAltLonLat[0], initialAltLonLat[1], initialAltLonLat[2]); // Convert the initial altitude, longitude, and latitude to ECEF
+            std::fill(std::begin(oldENU), std::end(oldENU), 0); // Initial old position set to 0
 
             sub = n.subscribe("/fix", 1, &pub_sub::callback, this);
             pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
         }
 
         void callback(const sensor_msgs::NavSatFix::ConstPtr &data) {
-            std::array<double, 3> newCoordinates = AltLonLatToECEF(data->altitude, data->longitude, data->latitude); // Gets the new ECEF coordinates
+            std::array<double, 3> newCoordinatesECEF = AltLonLatToECEF(data->altitude, data->longitude, data->latitude); // Gets the new ECEF coordinates
+            std::array<double, 3> newCoordinatesENU = ECEFToENU(newCoordinatesECEF[0], newCoordinatesECEF[1], newCoordinatesECEF[2]); // Gets the new ENU coordinates
+
+            nav_msgs::Odometry odom_msg; // Create new odometry message
+
+            odom_msg.pose.pose.position.x = newCoordinatesENU[0]; // Set message pose (x,y,z)
+            odom_msg.pose.pose.position.y = newCoordinatesENU[1];
+            odom_msg.pose.pose.position.z = newCoordinatesENU[2];
+
+            // Need to set orientation (turtle strat)
+
+            double theta = atan((newCoordinatesENU[1]-oldENU[1])/(newCoordinatesENU[0]-oldENU[0])); // Calculates the heading by look at the previous and current position
+            ROS_INFO("Heading: %lf", theta);
+
+            /*
+            tf::Quaternion q; // Create tf quaternion
+            q.setRPY(0, 0, theta); // Set orientation of quaternion
+
+            geometry_msgs::Quaternion q2; // Create other type of quaternion
+            quaternionMsgToTF(q2, q); // Create geometry_msgs quaternion from tf quaternion
+
+            odom_msg.pose.pose.orientation = q2;
+
+            pub.publish(odom_msg);
+            */
+
+            oldENU = newCoordinatesENU; // We update the previous ENU
         }
 
         //The 3 parameters of this method are in degrees
         std::array<double, 3> AltLonLatToECEF(double alt, double lon, double lat) {
-            ROS_INFO("Received parameters: alt: %lf, long: %lf, lat: %lf", alt, lon, lat); // TODO: Must delete
+            ROS_INFO("Received parameters in AltLonLatToECEF: alt: %lf, long: %lf, lat: %lf", alt, lon, lat); // TODO: Must delete
 
             std::array<double, 3> res;
 
@@ -57,9 +86,27 @@ class pub_sub {
             res[1] = (nResult + alt) * cos(latRad) * sin(lonRad);
             res[2] = (nResult * (1 - e_square) + alt) * sin(latRad);
 
-            ROS_INFO("Converted parameters: X: %lf, Y: %lf, Z: %lf", res[0], res[1], res[2]); // TODO: Must delete
+            ROS_INFO("Converted parameters in AltLonLatToECEF: X: %lf, Y: %lf, Z: %lf", res[0], res[1], res[2]); // TODO: Must delete
 
             return res; // Contains XECEF, YECEF, ZECEF
+        }
+
+        std::array<double, 3> ECEFToENU(double X, double Y, double Z) {
+            ROS_INFO("Received parameters in ECEFToENU: X: %lf, Y: %lf, Z: %lf", X, Y, Z); // TODO: Must delete
+
+            std::array<double, 3> res;
+
+            double deltaX = X-initialECEF[0]; // Xp - Xr
+            double deltaY = Y-initialECEF[1];
+            double deltaZ = Z-initialECEF[2];
+
+            res[0] = -sin(initialAltLonLat[1])*deltaX + cos(initialAltLonLat[1])*deltaY;
+            res[1] = -sin(initialAltLonLat[2])*cos(initialAltLonLat[1])*deltaX - sin(initialAltLonLat[2])*sin(initialAltLonLat[1])*deltaY + cos(initialAltLonLat[2])*deltaZ;
+            res[2] = cos(initialAltLonLat[2])*cos(initialAltLonLat[1])*deltaX + cos(initialAltLonLat[2])*sin(initialAltLonLat[1])*deltaY + sin(initialAltLonLat[2])*deltaZ;
+
+            ROS_INFO("Converted parameters in ECEFToENU: X: %lf, Y: %lf, Z: %lf", res[0], res[1], res[2]); // TODO: Must delete
+
+            return res; // Contains ENU coordinates
         }
 
         //The parameter is in radiants
